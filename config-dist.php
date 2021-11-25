c68c53bbda0e31dd782f063ff9239a6bc5c7bb09
Avi Levy <avi@sysbind.co.il>
2021-11-25 10:07:38 +0200

    MDL-63128 core: Add support for use redis cluster as session handler

--- config-dist.php
+++ config-dist.php
@@ -315,6 +315,7 @@ $CFG->admin = 'admin';
 //
 //   Redis session handler (requires redis server and redis extension):
 //      $CFG->session_handler_class = '\core\session\redis';
+//      $CFG->session_redis_clustermode = false;
 //      $CFG->session_redis_host = '127.0.0.1';
 //      $CFG->session_redis_port = 6379;  // Optional.
 //      $CFG->session_redis_database = 0;  // Optional, default is db 0.
