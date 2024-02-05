f5c7e45e49382aa696f1d2dcf8154e4b07bf2ed0
Asaf Ohayon <asaf@sysbind.co.il>
2024-02-05 18:50:28 +0200

    redis db driver: completed boilpirate code

--- lib/dml/redis_native_moodle_database.php
+++ lib/dml/redis_native_moodle_database.php
@@ -30,6 +30,10 @@ require_once(__DIR__.'/moodle_database.php');
  * Native redis class representing moodle database interface.
  */
 class redis_native_moodle_database extends moodle_database {
+
+    /** @var array $serverinfo cache */
+    private $serverinfo = [];
+
     /**
      * Detects if all needed PHP stuff installed.
      * Note: can be used before connect()
@@ -42,6 +46,24 @@ class redis_native_moodle_database extends moodle_database {
         return true;
     }
 
+    /**
+     * Connects to the database.
+     * Must be called before other methods.
+     * @param string $dbhost The database host.
+     * @param string $dbuser The database user to connect as.
+     * @param string $dbpass The password to use when connecting to the database.
+     * @param string $dbname The name of the database being connected to.
+     * @param mixed $prefix string means moodle db prefix, false used for external databases where prefix not used
+     * @param array $dboptions driver specific options
+     * @return bool true
+     * @throws dml_connection_exception if error
+     */
+    public function connect($dbhost, $dbuser, $dbpass, $dbname, $prefix, array $dboptions=null) {
+        $this->redis = new Redis('127.0.0.1');
+        $this->redis->connect();
+    }
+    
+    
     /**
      * Returns database family type - describes SQL dialect
      * Note: can be used before connect()
@@ -86,5 +108,440 @@ class redis_native_moodle_database extends moodle_database {
     public function get_configuration_help() {
         return get_string('nativeredishelp', 'install');
     }
+
+    /**
+     * Returns database server info array
+     * @return array Array containing 'description' and 'version' info
+     */
+    public function get_server_info(): array {
+        if (empty($this->serverinfo)) {
+            $this->query_start('--redis_version()', null, SQL_QUERY_AUX);
+            $this->serverinfo = ['server' => '7.0']; // TODO
+            $this->query_end(true);
+        }
+        return [
+            'description' => $this->serverinfo['server'],
+            'version' => $this->serverinfo['server'],
+        ];
+    }
+
+    /**
+     * Returns supported query parameter types
+     * @return int bitmask of accepted SQL_PARAMS_*
+     */
+    protected function allowed_param_types() {
+        return SQL_PARAMS_DOLLAR;
+    }
+
+    /**
+     * Returns last error reported by database engine.
+     * @return string error message
+     */
+    public function get_last_error() {
+        return "LAST_ERROR_NOT_IMPLEMENTED";
+    }
+
+    /**
+     * Return tables in database WITHOUT current prefix.
+     * @param bool $usecache if true, returns list of cached tables.
+     * @return array of table names in lowercase and without prefix
+     */
+    public function get_tables($usecache=true) {
+        if ($usecache and $this->tables !== null) {
+            return $this->tables;
+        }
+        $this->tables = ['NOT_YET_IMPLEMENTED'];
+
+        return $this->tables;
+    }
+
+    /**
+     * Return table indexes - everything lowercased.
+     * @param string $table The table we want to get indexes from.
+     * @return array of arrays
+     */
+    public function get_indexes($table) {
+        return ['NOT_YET_IMPLEMENTED'];
+    }
+
+    /**
+     * Returns detailed information about columns in table.
+     *
+     * @param string $table name
+     * @return database_column_info[] array of database_column_info objects indexed with column names
+     */
+    protected function fetch_columns(string $table): array {
+        return ['NOT_YET_IMPLEMENTED'];
+    }
+
+    /**
+     * Normalise values based in RDBMS dependencies (booleans, LOBs...)
+     *
+     * @param database_column_info $column column metadata corresponding with the value we are going to normalise
+     * @param mixed $value value we are going to normalise
+     * @return mixed the normalised value
+     */
+    protected function normalise_value($column, $value) {
+        $this->detect_objects($value);
+
+        return $value;
+    }
+
+    /**
+     * Do NOT use in code, to be used by database_manager only!
+     * @param string|array $sql query
+     * @param array|null $tablenames an array of xmldb table names affected by this request.
+     * @return bool true
+     * @throws ddl_change_structure_exception A DDL specific exception is thrown for any errors.
+     */
+    public function change_database_structure($sql, $tablenames = null) {
+        $this->get_manager(); // Includes DDL exceptions classes ;-)
+        if (is_array($sql)) {
+            $sql = implode("\n;\n", $sql);
+        }
+
+
+        throw new coding_exception("Redis -> change_database_structure not yet implemented");
+        
+        return true;
+    }
+
+    /**
+     * Execute general sql query. Should be used only when no other method suitable.
+     * Do NOT use this to make changes in db structure, use database_manager methods instead!
+     * @param string $sql query
+     * @param array $params query parameters
+     * @return bool true
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function execute($sql, array $params=null) {
+        list($sql, $params, $type) = $this->fix_sql_params($sql, $params);
+
+        throw new coding_exception("Redis -> execute not yet implemented");
+    }
+
+    /**
+     * Get a number of records as a moodle_recordset using a SQL statement.
+     *
+     * Since this method is a little less readable, use of it should be restricted to
+     * code where it's possible there might be large datasets being returned.  For known
+     * small datasets use get_records_sql - it leads to simpler code.
+     *
+     * The return type is like:
+     * @see function get_recordset.
+     *
+     * @param string $sql the SQL select query to execute.
+     * @param array $params array of sql parameters
+     * @param int $limitfrom return a subset of records, starting at this point (optional, required if $limitnum is set).
+     * @param int $limitnum return a subset comprising this many records (optional, required if $limitfrom is set).
+     * @return moodle_recordset instance
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function get_recordset_sql($sql, array $params=null, $limitfrom=0, $limitnum=0) {
+
+        list($limitfrom, $limitnum) = $this->normalise_limit_from_num($limitfrom, $limitnum);
+
+        throw new coding_exception("Redis -> get_recordset_sql not yet implemented");
+    }
+
+    /**
+     * Get a number of records as an array of objects using a SQL statement.
+     *
+     * Return value is like:
+     * @see function get_records.
+     *
+     * @param string $sql the SQL select query to execute. The first column of this SELECT statement
+     *   must be a unique value (usually the 'id' field), as it will be used as the key of the
+     *   returned array.
+     * @param array $params array of sql parameters
+     * @param int $limitfrom return a subset of records, starting at this point (optional, required if $limitnum is set).
+     * @param int $limitnum return a subset comprising this many records (optional, required if $limitfrom is set).
+     * @return array of objects, or empty array if no records were found
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function get_records_sql($sql, array $params = null, $limitfrom = 0, $limitnum = 0) {
+        list($limitfrom, $limitnum) = $this->normalise_limit_from_num($limitfrom, $limitnum);
+        
+        throw new coding_exception("Redis -> get_records_sql not yet implemented");        
+    }
+
+    /**
+     * Selects records and return values (first field) as an array using a SQL statement.
+     *
+     * @param string $sql The SQL query
+     * @param array $params array of sql parameters
+     * @return array of values
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function get_fieldset_sql($sql, array $params=null) {
+        list($sql, $params, $type) = $this->fix_sql_params($sql, $params);
+
+
+        throw new coding_exception("Redis -> get_fieldset_sql not yet implemented");        
+    }
+
+    /**
+     * Insert new record into database, as fast as possible, no safety checks, lobs not supported.
+     * @param string $table name
+     * @param mixed $params data record as object or array
+     * @param bool $returnit return it of inserted record
+     * @param bool $bulk true means repeated inserts expected
+     * @param bool $customsequence true if 'id' included in $params, disables $returnid
+     * @return bool|int true or new id
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function insert_record_raw($table, $params, $returnid=true, $bulk=false, $customsequence=false) {
+        if (!is_array($params)) {
+            $params = (array)$params;
+        }
+
+        throw new coding_exception("Redis -> insert_record_raw not yet implemented");        
+    }
+
+    /**
+     * Insert a record into a table and return the "id" field if required.
+     *
+     * Some conversions and safety checks are carried out. Lobs are supported.
+     * If the return ID isn't required, then this just reports success as true/false.
+     * $data is an object containing needed data
+     * @param string $table The database table to be inserted into
+     * @param object|array $dataobject A data object with values for one or more fields in the record
+     * @param bool $returnid Should the id of the newly created record entry be returned? If this option is not requested then true/false is returned.
+     * @return bool|int true or new id
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function insert_record($table, $dataobject, $returnid=true, $bulk=false) {
+        $dataobject = (array)$dataobject;
+
+        $columns = $this->get_columns($table);
+        if (empty($columns)) {
+            throw new dml_exception('ddltablenotexist', $table);
+        }
+
+        $cleaned = array();
+
+        foreach ($dataobject as $field=>$value) {
+            if ($field === 'id') {
+                continue;
+            }
+            if (!isset($columns[$field])) {
+                continue;
+            }
+            $column = $columns[$field];
+            $cleaned[$field] = $this->normalise_value($column, $value);
+        }
+
+        return $this->insert_record_raw($table, $cleaned, $returnid, $bulk);
+
+    }
+    
+    /**
+     * Import a record into a table, id field is required.
+     * Safety checks are NOT carried out. Lobs are supported.
+     *
+     * @param string $table name of database table to be inserted into
+     * @param object $dataobject A data object with values for one or more fields in the record
+     * @return bool true
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function import_record($table, $dataobject) {
+        $dataobject = (array)$dataobject;
+
+        $columns = $this->get_columns($table);
+        $cleaned = array();
+
+        foreach ($dataobject as $field=>$value) {
+            $this->detect_objects($value);
+            if (!isset($columns[$field])) {
+                continue;
+            }
+            $column = $columns[$field];
+            $cleaned[$field] = $this->normalise_value($column, $value);
+        }
+
+        return $this->insert_record_raw($table, $cleaned, false, true, true);
+    }
+
+
+    /**
+     * Update record in database, as fast as possible, no safety checks, lobs not supported.
+     * @param string $table name
+     * @param stdClass|array $params data record as object or array
+     * @param bool true means repeated updates expected
+     * @return bool true
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function update_record_raw($table, $params, $bulk=false) {
+        $params = (array)$params;
+
+        if (!isset($params['id'])) {
+            throw new coding_exception('moodle_database::update_record_raw() id field must be specified.');
+        }
+        $id = $params['id'];
+        unset($params['id']);
+
+        if (empty($params)) {
+            throw new coding_exception('moodle_database::update_record_raw() no fields found.');
+        }
+
+        throw new coding_exception("Redis -> update_record_raw not yet implemented");
+        
+        return true;
+    }
+
+    /**
+     * Update a record in a table
+     *
+     * $dataobject is an object containing needed data
+     * Relies on $dataobject having a variable "id" to
+     * specify the record to update
+     *
+     * @param string $table The database table to be checked against.
+     * @param stdClass|array $dataobject An object with contents equal to fieldname=>fieldvalue.
+     *        Must have an entry for 'id' to map to the table specified.
+     * @param bool true means repeated updates expected
+     * @return bool true
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function update_record($table, $dataobject, $bulk=false) {
+        $dataobject = (array)$dataobject;
+
+        $columns = $this->get_columns($table);
+        $cleaned = array();
+
+        foreach ($dataobject as $field=>$value) {
+            if (!isset($columns[$field])) {
+                continue;
+            }
+            $column = $columns[$field];
+            $cleaned[$field] = $this->normalise_value($column, $value);
+        }
+
+        $this->update_record_raw($table, $cleaned, $bulk);
+
+        return true;
+    }
+
+    /**
+     * Set a single field in every table record which match a particular WHERE clause.
+     *
+     * @param string $table The database table to be checked against.
+     * @param string $newfield the field to set.
+     * @param string $newvalue the value to set the field to.
+     * @param string $select A fragment of SQL to be used in a where clause in the SQL call.
+     * @param array $params array of sql parameters
+     * @return bool true
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function set_field_select($table, $newfield, $newvalue, $select, array $params=null) {
+
+        if ($select) {
+            $select = "WHERE $select";
+        }
+        if (is_null($params)) {
+            $params = array();
+        }
+        list($select, $params, $type) = $this->fix_sql_params($select, $params);
+        $i = count($params)+1;
+
+        // Get column metadata
+        $columns = $this->get_columns($table);
+        $column = $columns[$newfield];
+
+        $normalisedvalue = $this->normalise_value($column, $newvalue);
+
+        $newfield = "$newfield = \$" . $i;
+        $params[] = $normalisedvalue;
+        $sql = "UPDATE {$this->prefix}$table SET $newfield $select";
+
+        throw new coding_exception("Redis -> set_field_select not yet implemented");
+        
+        return true;
+    }
+
+    /**
+     * Delete one or more records from a table which match a particular WHERE clause, lobs not supported.
+     *
+     * @param string $table The database table to be checked against.
+     * @param string $select A fragment of SQL to be used in a where clause in the SQL call (used to define the selection criteria).
+     * @param array $params array of sql parameters
+     * @return bool true
+     * @throws dml_exception A DML specific exception is thrown for any errors.
+     */
+    public function delete_records_select($table, $select, array $params=null) {
+        if ($select) {
+            $select = "WHERE $select";
+        }
+        $sql = "DELETE FROM {$this->prefix}$table $select";
+
+        list($sql, $params, $type) = $this->fix_sql_params($sql, $params);
+
+        throw new coding_exception("Redis -> delete_records_select not yet implemented");
+
+        return true;
+    }
+    
+    public function sql_concat() {
+        $arr = func_get_args();
+        $s = implode(' || ', $arr);
+        if ($s === '') {
+            return " '' ";
+        }
+        // Add always empty string element so integer-exclusive concats
+        // will work without needing to cast each element explicitly
+        return " '' || $s ";
+    }
+
+    public function sql_concat_join($separator="' '", $elements=array()) {
+        for ($n=count($elements)-1; $n > 0 ; $n--) {
+            array_splice($elements, $n, 0, $separator);
+        }
+        $s = implode(' || ', $elements);
+        if ($s === '') {
+            return " '' ";
+        }
+        return " $s ";
+    }
+
+    /**
+     * Return SQL for performing group concatenation on given field/expression
+     *
+     * @param string $field
+     * @param string $separator
+     * @param string $sort
+     * @return string
+     */
+    public function sql_group_concat(string $field, string $separator = ', ', string $sort = ''): string {
+        $fieldsort = $sort ? "ORDER BY {$sort}" : '';
+        return "STRING_AGG(" . $this->sql_cast_to_char($field) . ", '{$separator}' {$fieldsort})";
+    }
+
+    /**
+     * Driver specific start of real database transaction,
+     * this can not be used directly in code.
+     * @return void
+     */
+    protected function begin_transaction() {
+        throw new coding_exception("Redis -> delete_records_select not yet implemented");        
+    }
+
+    /**
+     * Driver specific commit of real database transaction,
+     * this can not be used directly in code.
+     * @return void
+     */
+    protected function commit_transaction() {
+        throw new coding_exception("Redis -> delete_records_select not yet implemented");
+    }
+
+    /**
+     * Driver specific abort of real database transaction,
+     * this can not be used directly in code.
+     * @return void
+     */
+    protected function rollback_transaction() {
+        throw new coding_exception("Redis -> delete_records_select not yet implemented");
+    }   
     
 }
+
